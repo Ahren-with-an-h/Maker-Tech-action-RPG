a11296293c0328343cce014bb388e8b485e941f4
Ahren Groesch <agroesch@gmail.com>
2024-01-22 23:46:55 -0500

    Inventory Gui

---
+++ scenes/CanvasLayer.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@onready var inventory = $IneventoryGui
+
+	 
+func _ready():
+	inventory.close()
+	
+
+func _input(event):
+	if event.is_action_pressed("toggle_inventory"):
+		if inventory.isOpen:
+			inventory.close()
+		else:
+			inventory.open()
+		
+		

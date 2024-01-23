59dbb2a1d8ffa095275cd985d80f510e0d033e3a
Ahren Groesch <agroesch@gmail.com>
2024-01-22 21:05:11 -0500

    Collectibles

--- player/player.gd
+++ player/player.gd
@@ -4,11 +4,22 @@ class_name Player
 
 signal health_changed
 
-@export var speed: int = 200
+@export var speed: int = 50
 @onready var animations = $AnimationPlayer
+@onready var effects = $Effects
+@onready var hurt_box = $HurtBox
+@onready var hurt_timer = $HurtTimer
 
 @export var max_health = 3
 var health: int = max_health
+
+@export var knockback_power: int = 5000
+
+var is_hurt: bool = false
+
+
+func _ready():
+	effects.play("RESET")
 
 
 func handle_input():
@@ -53,12 +64,45 @@ func _physics_process(delta):
 	move_and_slide()
 	handle_collision()
 	update_animation()
+	if !is_hurt:
+		for area in hurt_box.get_overlapping_areas():
+			if area.name == "HitBox":
+				hurt_by_enemy(area)
 
+
+func hurt_by_enemy(area):
+	health -= 1
+	if health < 0:
+		health = max_health
+	
+	health_changed.emit(health)
+	is_hurt = true
+	
+	knockback(area.get_parent().velocity)
+	effects.play("hurt_blink")
+	hurt_timer.start()
+	await hurt_timer.timeout
+	effects.play("RESET")
+	is_hurt = false
+	
 
 func _on_hurt_box_area_entered(area):
-	if area.name == "HitBox":
-		health -= 1
-		if health < 0:
-			health = max_health
-		
-		health_changed.emit(health)
+	if area.has_method("collect"):
+		area.collect()
+
+
+func _on_hurt_box_area_exited(area):
+	pass
+	
+	
+func knockback(enemy_velocity):
+	var knockback_dir = (enemy_velocity - velocity).normalized() * knockback_power
+	velocity = knockback_dir
+	print_debug(velocity)
+	print_debug(position)
+	move_and_slide()
+	print_debug(position)
+	print_debug(" ")
+
+
+
